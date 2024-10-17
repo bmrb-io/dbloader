bce1d26f83706e3c8030c24aed9f3b79d500f999
Jon Wedell <jon@jonwedell.com>
2024-10-17 18:30:43 +0900

    Fix str/bytes issue

--- loader/__init__.py
+++ loader/__init__.py
@@ -121,22 +121,22 @@ def runscript( dsn, script, verbose = False ) :
         sys.stderr.write( "\n" )
         sys.stderr.write( "** STDERR **\n" )
         for i in err.splitlines() :
-            sys.stderr.write( i.decode() )
+            sys.stderr.write( i )
             sys.stderr.write( "\n" )
         sys.stderr.write( "** STDOUT **\n" )
         for i in out.splitlines() :
-            sys.stderr.write( i.decode() )
+            sys.stderr.write( i )
             sys.stderr.write( "\n" )
         return False
 
     if verbose :
         sys.stderr.write( "** STDERR **\n" )
         for i in err.splitlines() :
-            sys.stderr.write( i.decode() )
+            sys.stderr.write( i )
             sys.stderr.write( "\n" )
         sys.stderr.write( "** STDOUT **\n" )
         for i in out.splitlines() :
-            sys.stderr.write( i.decode() )
+            sys.stderr.write( i )
             sys.stderr.write( "\n" )
 
     return True
@@ -185,22 +185,22 @@ def add_ro_grants( dsn, schema, user, verbose = False ) :
             sys.stderr.write( "\n" )
             sys.stderr.write( "** STDERR **\n" )
             for i in err.splitlines() :
-                sys.stderr.write( i.decode() )
+                sys.stderr.write( i )
                 sys.stderr.write( "\n" )
             sys.stderr.write( "** STDOUT **\n" )
             for i in out.splitlines() :
-                sys.stderr.write( i.decode() )
+                sys.stderr.write( i )
                 sys.stderr.write( "\n" )
             return False
 
         if verbose :
             sys.stderr.write( "** STDERR **\n" )
             for i in err.splitlines() :
-                sys.stderr.write( i.decode() )
+                sys.stderr.write( i )
                 sys.stderr.write( "\n" )
             sys.stderr.write( "** STDOUT **\n" )
             for i in out.splitlines() :
-                sys.stderr.write( i.decode() )
+                sys.stderr.write( i )
                 sys.stderr.write( "\n" )
 
     return True
